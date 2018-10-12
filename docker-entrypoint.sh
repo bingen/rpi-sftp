e5f78a60b8869822023fa0740cf7148647dc48c2
ßingen <bingentxu@gmail.com>
2018-10-12 23:18:08 +0200

    Add Server Public key to entry point script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,6 +14,11 @@ useradd -d ${PAPERLESS_CONSUMPTION_DIR} -p `openssl passwd -1 ${PAPERLESS_FTP_PW
 chown ${PAPERLESS_FTP_USER} ${PAPERLESS_CONSUMPTION_DIR}
 chmod 777 ${PAPERLESS_CONSUMPTION_DIR}
 
+# Copy Server Public key if any (this is needed at least for Brother ADS-2400n)
+if [[ -s ${PAPERLESS_CONSUMPTION_DIR}/ssh_host_rsa_key.pub ]]; then
+    cp ${PAPERLESS_CONSUMPTION_DIR}/ssh_host_rsa_key.pub /etc/ssh/;
+fi
+
 # https://bugs.launchpad.net/ubuntu/+source/openssh/+bug/45234
 mkdir -p /var/run/sshd
 
